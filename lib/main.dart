6cfb5afcc8cdcc012206b025d00e742a8dd0c560
gopinathnelluri <nathgopinathgopi@gmail.com>
2019-05-03 20:12:12 -0400

    Teddy animated login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,135 @@
 import 'package:flutter/material.dart';
-import 'screens/home.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'flare/teddy_controller.dart';
+import 'util/tracking_text_input.dart';
+import 'util/signin_button.dart';
 
-void main() => runApp(HelloFlutterApp());
+void main() => runApp(MyApp());
 
-
-class HelloFlutterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        title: 'Flutter 01',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Home()
-        );
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TeddyController _teddyController;
+  initState() {
+    _teddyController = TeddyController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    EdgeInsets devicePadding = MediaQuery.of(context).padding;
+
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(93, 142, 155, 1.0),
+        body: Container(
+            child: Stack(children: <Widget>[
+            Positioned.fill(
+                child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [0.0, 1.0],
+                    colors: [
+                      Color.fromRGBO(170, 207, 211, 1.0),
+                      Color.fromRGBO(93, 142, 155, 1.0),
+                    ],
+                  )
+                ),
+              )
+            ),
+            Positioned.fill(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: devicePadding.top + 50.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      padding: const EdgeInsets.only(left: 30.0, right:30.0),
+                      child: FlareActor(
+                        "assets/flr/teddy.flr",
+                        shouldClip: false,
+                        alignment: Alignment.bottomCenter,
+                        fit: BoxFit.contain,
+                        controller: _teddyController,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Form(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              TrackingTextInput(
+                                    label: "Email",
+                                    hint: "What's your email address?",
+                                    onCaretMoved: (Offset caret) {
+                                      _teddyController.lookAt(caret);
+                                    }),
+                              TrackingTextInput(
+                                    label: "Password",
+                                    hint: "Try 'bears'...",
+                                    isObscured: true,
+                                    onCaretMoved: (Offset caret) {
+                                      _teddyController.coverEyes(caret != null);
+                                      _teddyController.lookAt(null);
+                                    },
+                                    onTextChanged: (String value) {
+                                      _teddyController.setPassword(value);
+                                    },
+                              ),
+                              SigninButton(
+                                    child: Text("Sign In",
+                                        style: TextStyle(
+                                            fontFamily: "RobotoMedium",
+                                            fontSize: 16,
+                                            color: Colors.white)),
+                                    onPressed: () {
+                                      _teddyController.submitPassword();
+                                    }
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    )
+                  ]
+                ),
+              ),
+            )
+          ]
+        )
+      )
+    );
   }
 }
